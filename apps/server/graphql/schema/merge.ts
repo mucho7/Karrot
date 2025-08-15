24894bb10ca695fc844aa5c8bebbee9743b9d5ac
Yadon tastes good <109324468+mucho7@users.noreply.github.com>
2025-08-16 01:58:05 +0900

    Revert "Docs/week1 day2 (#3)" (#4)
    This reverts commit 58ae30cd65aea683ed26c6fae61e5d0519e006e7.

--- apps/server/graphql/schema/merge.ts
+++
@@ -1,6 +0,0 @@
-import { loadFilesSync } from '@graphql-tools/load-files';
-import { mergeTypeDefs } from '@graphql-tools/merge';
-
-export const typeDefs = mergeTypeDefs(
-  loadFilesSync('./graphql/schema/**/*.graphql', { extensions: ['graphql'] })
-);
